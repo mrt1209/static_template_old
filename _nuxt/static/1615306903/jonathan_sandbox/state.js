99772033c61c8188a20a26ec89c4e5be5192dcd6
mrt1209 <mrt1209@gmail.com>
2021-03-09 11:27:31 -0500

    Updates

--- _nuxt/static/1615306903/jonathan_sandbox/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstatic_template\u002F_nuxt\u002Fstatic\u002F1615306903",layout:"default",error:a,serverRendered:true,routePath:"\u002Fjonathan_sandbox",config:{app:{basePath:"\u002Fstatic_template\u002F",assetsPath:"\u002Fstatic_template\u002F_nuxt\u002F",cdnURL:a}}}}(null));
